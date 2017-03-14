5a4755663a18cd081673dfc0b5b8ad17767fc954
Java <Java>
2017-03-14 10:09:37 +0200

    version 1.0

---
+++ src/ua/itea/Dog.java
@@ -0,0 +1,18 @@
+package ua.itea;
+
+/**
+ * Created by Java base on 14.03.2017.
+ */
+public class Dog {
+    String name = "Krevedko";
+    int age = 12;
+
+    public Dog() {
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
